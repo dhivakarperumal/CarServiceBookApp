0928e6b4a120fe5d6d30078904bdbcfc279153f8
Dhivakar <dhivakarp305@gmail.com>
2026-04-01 17:07:02 +0530

    fix it

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -37,8 +37,8 @@ export default function AuthLayout() {
         },
       }}
     >
-      <Stack.Screen name="login" options={{ title: 'Login' }} />
       <Stack.Screen name="register" options={{ title: 'Register' }} />
+      <Stack.Screen name="login" options={{ title: 'Login' }} />
     </Stack>
   );
 }
