e6ae128045020778db6169e3a9e7c8973a3a01e6
qtechx001 <qtechx001@gmail.com>
2026-04-11 11:18:33 +0530

    Error Fix

--- app/(adminPages)/add-car-service.tsx
+++ app/(adminPages)/add-car-service.tsx
@@ -293,7 +293,6 @@ export default function AddCarService() {
                   </Text>
                )}
             </TouchableOpacity>
-          </View>
         </ScrollView>
       </KeyboardAvoidingView>
     </SafeAreaView>
